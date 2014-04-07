e7e1f6477e5c336a012642a775ff3b83b4f454dc
Gustaf Mossakowski <web@koenige.org>
2014-04-07 20:54:08 +0000

    module default: logging modified to support small screens
    git-svn-id: file:///Repos/modules/default/trunk@23 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/database_logging.php
+++ zzbrick_tables/database_logging.php
@@ -25,18 +25,22 @@ $zz['fields'][1]['field_name'] = 'log_id';
 $zz['fields'][1]['type'] = 'id';
 
 $zz['fields'][2]['field_name'] = 'query';
+$zz['fields'][2]['class'] = 'block480a';
 
 if (!empty($zz_conf['logging_id'])) {
 	$zz['fields'][3]['title'] = 'Record';
 	$zz['fields'][3]['field_name'] = 'record_id';
 	$zz['fields'][3]['type'] = 'number';
+	$zz['fields'][3]['class'] = 'hidden480';
 }
 
 $zz['fields'][4]['field_name'] = 'user';
+$zz['fields'][4]['class'] = 'hidden480';
 
 $zz['fields'][99]['field_name'] = 'last_update';
 $zz['fields'][99]['type'] = 'display';
 $zz['fields'][99]['type_detail'] = 'timestamp';
+$zz['fields'][99]['class'] = 'block480';
 
 $zz['sql'] = 'SELECT * FROM '.$zz_conf['logging_table'];
 $zz['sqlorder'] = ' ORDER BY log_id DESC';
@@ -46,5 +50,3 @@ $zz_conf['limit'] = 20;
 $zz_conf['add'] = false;
 $zz_conf['edit'] = false;
 $zz_conf['delete'] = false;
-
-?>
