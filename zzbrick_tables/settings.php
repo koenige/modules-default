40c0e63e6dd6a4fea5e802eece37c19db681e2d9
Gustaf Mossakowski <web@koenige.org>
2014-11-06 13:52:07 +0000

    default-module settings: updated table arrangement
    git-svn-id: file:///Repos/modules/default/trunk@35 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/settings.php
+++ zzbrick_tables/settings.php
@@ -30,17 +30,18 @@ $zz['fields'][2]['search'] = '/*_PREFIX_*/logins.username';
 
 $zz['fields'][3]['title'] = 'Key';
 $zz['fields'][3]['field_name'] = 'setting_key';
-
-$zz['fields'][4]['title'] = 'Value';
-$zz['fields'][4]['field_name'] = 'setting_value';
-$zz['fields'][4]['null'] = true;
-$zz['fields'][4]['null_string'] = true;
+$zz['fields'][3]['list_append_next'] = true;
 
 $zz['fields'][6]['field_name'] = 'explanation';
 $zz['fields'][6]['type'] = 'memo';
 $zz['fields'][6]['rows'] = 3;
 $zz['fields'][6]['list_prefix'] = '<p class="explanation" style="margin: .75em 2.5em; max-width: 40em; "><em>';
 $zz['fields'][6]['list_suffix'] = '</em></p>';
+
+$zz['fields'][4]['title'] = 'Value';
+$zz['fields'][4]['field_name'] = 'setting_value';
+$zz['fields'][4]['null'] = true;
+$zz['fields'][4]['null_string'] = true;
 
 $zz['sql'] = 'SELECT /*_PREFIX_*/_settings.*, /*_PREFIX_*/logins.username
 	FROM /*_PREFIX_*/_settings
