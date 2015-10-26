228be93dc5ae88a039b56968613712cf59afff3e
Gustaf Mossakowski <web@koenige.org>
2015-10-26 22:09:42 +0000

    zzform maintenance: include zzform-head via template, do not include all tables/common anymore
    git-svn-id: file:///Repos/modules/default/trunk@51 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_request/maintenance.inc.php
+++ zzbrick_request/maintenance.inc.php
@@ -45,11 +45,9 @@ function mod_default_maintenance($params) {
 	if (file_exists($file = $zz_setting['custom'].'/zzbrick_tables/_common.inc.php')) {
 		require_once $file;
 	}
-	foreach ($zz_setting['modules'] as $module) {
-		if (file_exists($file = $zz_setting['modules_dir'].'/'.$module.'/zzbrick_tables/_common.inc.php'))
-		 	require_once $file;
-	}
 	if (isset($brick['page'])) $page = $brick['page'];
+	$page['head'] = isset($page['head']) ? $page['head'] : '';
+	$page['head'] .= wrap_template('zzform-head', $zz_setting);
 
 	if (!empty($_SESSION) AND empty($zz_conf['user']) AND !empty($zz_setting['brick_username_in_session']))
 		$zz_conf['user'] = $_SESSION[$zz_setting['brick_username_in_session']];
