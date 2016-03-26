cf93d882cb7b0f3c9128a1425f0b37f4d889c338
Gustaf Mossakowski <web@koenige.org>
2016-03-26 12:09:10 +0000

    URIs: change character encoding to UTF-8
    git-svn-id: file:///Repos/modules/default/trunk@76 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/uris.php
+++ zzbrick_tables/uris.php
@@ -3,19 +3,19 @@
 /**
  * default module
  * Database table for list of URIs
- * DB-Tabelle für Liste von URIs
+ * DB-Tabelle fÃ¼r Liste von URIs
  *
- * Part of »Zugzwang Project«
+ * Part of Â»Zugzwang ProjectÂ«
  * http://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2012-2015 Gustaf Mossakowski
+ * @copyright Copyright Â© 2012-2015 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
 // access restriction has to be set in the file including this file
-// Bitte Zugriffsbeschränkungen in der Datei, die diese einbindet, definieren!
+// Bitte ZugriffsbeschrÃ¤nkungen in der Datei, die diese einbindet, definieren!
 
 $zz['title'] = 'URIs';
 $zz['table'] = '/*_PREFIX_*/_uris';
