a7bfda3eefa4676aee3f1ffbd2c1305dc36a3935
Gustaf Mossakowski <web@koenige.org>
2018-08-03 12:53:08 +0000

    add tfpdf to default library, extend it to use zzwrap's error handling
    git-svn-id: file:///Repos/modules/default/trunk@182 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ libraries/tfpdf.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * default module
+ * use tFPDF with zzwrap error logging
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+if (file_exists($zz_setting['lib'].'/tfpdf/src/tFPDF.php')) {
+	require_once $zz_setting['lib'].'/tfpdf/src/tFPDF.php';
+} else {
+	require_once $zz_setting['lib'].'/tfpdf/tFPDF.php';
+}
+
+
+class zzTFPDF extends tFPDF {
+	function Error($msg) {
+		// Fatal error
+		wrap_error(sprintf('FPDF error: %s', $msg), E_USER_ERROR);
+	}
+}
