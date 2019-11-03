8ce92c14f983b082d5d0c7093bf4117ce91db316
Gustaf Mossakowski <gustaf@koenige.org>
2019-11-03 14:04:00 +0000

    add details link to translation values from translations table
    git-svn-id: file:///Repos/modules/default/trunk@212 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/database_translations.php
+++ zzbrick_tables/database_translations.php
@@ -9,7 +9,7 @@
  * http://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2005-2010 Gustaf Mossakowski
+ * @copyright Copyright © 2005-2010, 2014, 2019 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -34,7 +34,7 @@ $zz['fields'][3]['title'] = 'Table';
 $zz['fields'][3]['field_name'] = 'table_name';
 if (!empty($_POST['db_name'])) {
 	$zz['fields'][3]['type'] = 'select';	
-	$zz['fields'][3]['sql'] = 'SHOW TABLES FROM '.$_POST['db_name'];
+	$zz['fields'][3]['sql'] = sprintf('SHOW TABLES FROM %s', wrap_db_escape($_POST['db_name']));
 } else
 	$zz['fields'][3]['type'] = 'text';	
 $zz['fields'][3]['list_append_next'] = true;
@@ -44,17 +44,33 @@ $zz['fields'][4]['title'] = 'Field';
 $zz['fields'][4]['field_name'] = 'field_name';
 if (!empty($_POST['db_name']) AND !empty($_POST['table_name'])) {
 	$zz['fields'][4]['type'] = 'select';
-	$zz['fields'][4]['sql'] = 'SHOW COLUMNS FROM '.$_POST['db_name'].'.'.$_POST['table_name'];
+	$zz['fields'][4]['sql'] = sprintf('SHOW COLUMNS FROM %s.%s', wrap_db_escape($_POST['db_name']), wrap_db_escape($_POST['table_name']));
 	$zz['fields'][4]['sql_index_only'] = true;
 } else
 	$zz['fields'][4]['type'] = 'text';
 	
 $zz['fields'][9]['title'] = 'Data type';
 $zz['fields'][9]['field_name'] = 'field_type';		
-$zz['fields'][9]['type'] = 'select';	
-$zz['fields'][9]['enum'] = array('varchar', 'text');
+$zz['fields'][9]['type'] = 'select';
+$zz['fields'][9]['enum_title'] = ['text line', 'memo'];
+$zz['fields'][9]['enum'] = ['varchar', 'text'];
 	
-$zz['sql'] = 'SELECT * FROM '.$zz_conf['translations_table'].'
-	ORDER BY db_name, table_name, field_name';
+$zz['sql'] = sprintf('SELECT * FROM %s
+	ORDER BY db_name, table_name, field_name'
+	, $zz_conf['translations_table']
+);
 
 $zz_conf['max_select'] = 100;
+
+$zz_conf['details'][1]['title'] = 'Translations';
+$zz_conf['details'][1]['referer'] = 1;
+$zz_conf['if'][1]['details'][1]['link'] = 'translations-varchar?where[translationfield_id]=';
+$zz_conf['if'][1]['details'][1]['sql'] = 'SELECT COUNT(*) FROM /*_PREFIX_*/_translations_varchar WHERE translationfield_id = %d';
+$zz_conf['if'][2]['details'][1]['link'] = 'translations-text?where[translationfield_id]=';
+$zz_conf['if'][2]['details'][1]['sql'] = 'SELECT COUNT(*) FROM /*_PREFIX_*/_translations_text WHERE translationfield_id = %d';
+
+$zz['conditions'][1]['scope'] = 'record';
+$zz['conditions'][1]['where'] = 'field_type = "varchar"';
+
+$zz['conditions'][2]['scope'] = 'record';
+$zz['conditions'][2]['where'] = 'field_type = "text"';
