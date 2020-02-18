0d1de7cbd4ba664fb68905b04c4231ee20fef510
Gustaf Mossakowski <gustaf@koenige.org>
2020-02-18 21:46:03 +0000

    add setting no_timeframe to password change form
    git-svn-id: file:///Repos/modules/default/trunk@218 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_forms/my-password.php
+++ zzbrick_forms/my-password.php
@@ -8,7 +8,7 @@
  * http://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2015, 2018 Gustaf Mossakowski
+ * @copyright Copyright © 2015, 2018, 2020 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -64,6 +64,7 @@ if (!empty($_GET['url'])) {
 	$zz_conf['redirect']['successful_update'] = $_GET['url'];
 }
 $zz_conf['text']['--']['Edit a record'] = 'Change My Password';
+$zz_conf['no_timeframe'] = true;
 
 function mod_default_password_update() {
 	if (empty($_SESSION['dont_require_old_password'])) return;
