ceed2d9bca21c64a95ee0c345a4f32d44251bef0
Gustaf Mossakowski <gustaf@koenige.org>
2020-02-25 21:20:18 +0000

    remove _settings.login_id from table scripts as well; remove forms/settings-users, reduce forms/settings-website
    git-svn-id: file:///Repos/modules/default/trunk@220 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_forms/settings-website.php
+++ zzbrick_forms/settings-website.php
@@ -8,21 +8,9 @@
  * http://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2014-2015, 2018 Gustaf Mossakowski
+ * @copyright Copyright © 2014-2015, 2018, 2020 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
 require __DIR__.'/../zzbrick_tables/settings.php';
-
-$zz['title'] = 'Website settings';
-
-unset($zz['fields'][2]); // login_id
-
-$zz['fields'][3]['class'] = 'block480a';
-$zz['fields'][4]['list_append_next'] = true;
-$zz['fields'][4]['class'] = 'block480 hyphenate';
-
-$zz['sql'] = 'SELECT /*_PREFIX_*/_settings.*
-	FROM /*_PREFIX_*/_settings';
-$zz['sqlorder'] = ' ORDER BY setting_key, setting_value';
