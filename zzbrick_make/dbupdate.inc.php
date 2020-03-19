ba91ac9b2ef5b9f8dddd097fe9ecea0d8fe323fd
Gustaf Mossakowski <gustaf@koenige.org>
2020-03-19 19:24:10 +0000

    add database update make script, reads update.sql per module and returns them as list
    git-svn-id: file:///Repos/modules/default/trunk@224 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_make/dbupdate.inc.php
@@ -0,0 +1,73 @@
+<?php 
+
+/**
+ * default module
+ * update database structure/content
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * update database structure and content that is related to the structure
+ * based on update.sql per module
+ *
+ * @param array $params
+ * @global array $zz_setting
+ * @return array $page
+ *		'text' => page content, 'title', 'breadcrumbs', ...
+ */
+function mod_default_make_dbupdate($params) {
+	global $zz_setting;
+
+	// look for update.sql
+	$data = [];
+	$file_template = $zz_setting['modules_dir'].'/%s/docs/sql/update.sql';
+	foreach ($zz_setting['modules'] as $module) {
+		$file = sprintf($file_template, $module);
+		if (!file_exists($file)) continue;
+		$data = array_merge($data, mod_default_make_dbupdate_readfile($file, $module));
+	}
+	ksort($data);
+	$data = array_values($data);
+
+	$page['text'] = wrap_template('dbupdate', $data);
+	$page['title'] = wrap_text('Database Updates');
+	$page['breadcrumbs'][] = wrap_text('Database Updates');
+	return $page;
+}
+
+/**
+ * read a .sql file and interpret it
+ *
+ * @param string $filename
+ * @return array
+ */
+function mod_default_make_dbupdate_readfile($filename, $module) {
+	$data = [];
+	$lines = file($filename);
+	foreach ($lines as $line) {
+		$line = trim($line);
+		if (!$line) continue;
+		if (substr($line, 0, 3) === '/**') continue;
+		if (substr($line, 0, 1) === '*') continue;
+		if (substr($line, 0, 2) === '*/') continue;
+		$line = explode("\t", $line);
+		$line[0] = ltrim($line[0], '/* ');
+		$line[0] = rtrim($line[0], ' */');
+		$line[0] = explode('-', $line[0]);
+		$key = vsprintf('%04d-%02d-%02d-%06d', $line[0]);
+		$data[$key] = [
+			'query' => trim($line[1]),
+			'module' => $module,
+			'date' => vsprintf('%04d-%02d-%02d', $line[0]),
+			'key' => $key
+		];
+	}
+	return $data;
+}
