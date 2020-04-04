77d1949bf1bf1130ba007aa371cf67378fd7cfe5
Gustaf Mossakowski <gustaf@koenige.org>
2020-04-04 12:04:30 +0000

    fix update.sql, key is already deleted when field is dropped
    git-svn-id: file:///Repos/modules/default/trunk@226 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- docs/sql/update.sql
+++ docs/sql/update.sql
@@ -14,6 +14,6 @@
 /* 2020-02-25-2 */	ALTER TABLE `redirects` CHANGE `old_url` `old_url` varchar(127) COLLATE 'latin1_general_ci' NOT NULL AFTER `redirect_id`, CHANGE `new_url` `new_url` varchar(127) COLLATE 'latin1_general_ci' NOT NULL AFTER `old_url`;
 /* 2020-02-25-3 */	ALTER TABLE `_settings` CHANGE `setting_value` `setting_value` varchar(750) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `setting_key`;
 /* 2020-02-25-4 */	ALTER TABLE `_settings` DROP `login_id`;
-/* 2020-02-25-5 */	ALTER TABLE `_settings` ADD UNIQUE (`setting_key`), DROP INDEX `login_id`;
+/* 2020-02-25-5 */	ALTER TABLE `_settings` ADD UNIQUE (`setting_key`);
 /* 2020-02-25-6 */	DELETE FROM _relations WHERE master_table = "logins" AND detail_table = "_settings";
 /* 2020-02-25-7 */	ALTER TABLE `webpages` ADD UNIQUE `identifier` (`identifier`);
