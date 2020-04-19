35e0b1bdd80f9af15a834c6f111d7a3475a7ddb0
Gustaf Mossakowski <gustaf@koenige.org>
2020-04-19 11:23:37 +0000

    add library script for phpmailer
    git-svn-id: file:///Repos/modules/default/trunk@239 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ libraries/phpmailer.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * default module
+ * Use 'phpmailer'
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+// remind that use is only possible globally, so add these prefixes
+
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\SMTP;
+//use PHPMailer\PHPMailer\Exception;
+
+require_once $zz_setting['lib'].'/phpmailer/src/Exception.php';
+require_once $zz_setting['lib'].'/phpmailer/src/PHPMailer.php';
+require_once $zz_setting['lib'].'/phpmailer/src/SMTP.php';
