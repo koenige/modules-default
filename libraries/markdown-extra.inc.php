15dba8c1715a0c948c8056af41c78e57a5950960
Gustaf Mossakowski <gustaf@koenige.org>
2021-02-16 10:16:06 +0000

    add functions markdown_inline() for inline text, markdown_attribute() for attribute text
    git-svn-id: file:///Repos/modules/default/trunk@378 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- libraries/markdown-extra.inc.php
+++ libraries/markdown-extra.inc.php
@@ -24,3 +24,18 @@ function markdown($text) {
 	$html = MarkdownExtra::defaultTransform($text);
 	return $html;	
 }
+
+function markdown_inline($text) {
+	$html = MarkdownExtra::defaultTransform($text);
+	$html = trim($html);
+	if (substr($html, 0, 3) !== '<p>') return $html;
+	if (substr($html, -4) !== '</p>') return $html;
+	$html = substr($html, 3, -4);
+	return $html;	
+}
+
+function markdown_attribute($text) {
+	$text = markdown_inline($text);
+	$text = strip_tags($text);
+	return $text;
+}
