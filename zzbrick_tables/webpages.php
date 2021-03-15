19073764799602dc51ac1951ad6c8e6e90e1d721
Gustaf Mossakowski <gustaf@koenige.org>
2021-03-15 20:09:20 +0000

    webpages: change parameter to edit a webpage to edit=admin
    git-svn-id: file:///Repos/modules/default/trunk@398 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/webpages.php
+++ zzbrick_tables/webpages.php
@@ -132,8 +132,8 @@ $zz_conf['copy'] = true;
 
 if (!wrap_access('default_webpages_admin')) {
 	unset($zz['fields'][15]); // no parameters
-	$zz['sql'] .= ' WHERE (ISNULL(webpages.parameters) OR !LOCATE("access=admin", webpages.parameters))
-		AND (ISNULL(main_pages.parameters) OR !LOCATE("access=admin", main_pages.parameters))';
+	$zz['sql'] .= ' WHERE (ISNULL(webpages.parameters) OR !LOCATE("edit=admin", webpages.parameters))
+		AND (ISNULL(main_pages.parameters) OR !LOCATE("edit=admin", main_pages.parameters))';
 }
 
 if (!empty($zz_setting['multiple_websites'])) {
