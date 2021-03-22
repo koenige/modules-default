dfdea55a852bff2dff5241d3b9492498188f0a1b
Gustaf Mossakowski <gustaf@koenige.org>
2021-03-22 12:57:29 +0000

    nocache: add setting js_css_nocache (timestamp) to dynamically append ?nocache query string in timestamp between timestamp and cache_control_text
    git-svn-id: file:///Repos/modules/default/trunk@401 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_page/nocache.inc.php
@@ -0,0 +1,35 @@
+<?php 
+
+/**
+ * default module
+ * check if JS or CSS files should not be cached
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2021 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * check if JS or CSS files should not be cached
+ *
+ * @param $params void
+ * @return array $page
+ */
+function page_nocache($params) {
+	global $zz_setting;
+	if (empty($zz_setting['js_css_nocache'])) return '';
+
+	// readable timestamp?
+	$timestamp = strtotime($zz_setting['js_css_nocache']);
+	if (!$timestamp) return '';
+
+	$now = time();
+	if ($timestamp + $zz_setting['cache_control_text'] > $now)
+		return '?nocache';
+
+	return '';
+}
