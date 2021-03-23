80fe856ed404babec1b06dd9af1c6583bb83d1f4
Gustaf Mossakowski <gustaf@koenige.org>
2021-03-23 12:28:37 +0000

    cfgfile: add function to export data from database as .cfg file, set queries via system.sql
    git-svn-id: file:///Repos/modules/default/trunk@402 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_request/cfgfile.inc.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * default module
+ * show data from database as .cfg file
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2021 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+function mod_default_cfgfile($params) {
+	// does query exist?
+	wrap_sql('cfg', 'set');
+	$sql = wrap_sql('data_'.$params[0]);
+	if (!$sql) return false;
+
+	// get raw data
+	$raw = wrap_db_fetch($sql, 'identifier');
+	$raw = array_values($raw);
+
+	$data = [];
+	foreach ($raw as $index => $entry) {
+		$data[$index]['identifier'] = $entry['identifier'];
+		if (!empty($entry['parameters'])) {
+			parse_str($entry['parameters'], $parameters);
+			$entry += $parameters;
+		}
+		foreach ($entry as $key => $value) {
+			if (in_array($key, ['identifier', 'parameters'])) continue;
+			if (!$value AND $value !== 0 AND $value !== '0') continue;
+			if (wrap_substr($key, '_list', 'end')) {
+				$value = explode(',', $value);
+				foreach ($value as $subkey => $subval) {
+					$data[$index]['keys'][] = [
+						'key' => $key.'['.$subkey.']', 'value' => $subval
+					];
+				}
+			} elseif (is_array($value)) {
+				foreach ($value as $subkey => $subval) {
+					$data[$index]['keys'][] = [
+						'key' => $key.'['.$subkey.']', 'value' => $subval
+					];
+				}
+			} else {
+				$data[$index]['keys'][] = [
+					'key' => $key, 'value' => $value
+				];
+			}
+		}
+	}
+	
+	$page['text'] = wrap_template('cfgfile', $data);
+	$page['content_type'] = 'txt';
+	return $page;
+}
