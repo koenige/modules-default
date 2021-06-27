01f45648d4ea2c64d9a69a50f77e6bb0cb736b7e
Gustaf Mossakowski <gustaf@koenige.org>
2021-06-27 10:53:39 +0000

    errorlogs: don’t show full path to log_dir if log file is inside log_dir
    git-svn-id: file:///Repos/modules/default/trunk@441 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_request/errorlogs.inc.php
+++ zzbrick_request/errorlogs.inc.php
@@ -19,32 +19,40 @@
  * @return array
  */
 function mod_default_errorlogs() {
+	global $zz_setting;
 	global $zz_conf;
 
 	if (!empty($zz_conf['error_handling'])) {
 		$data['error_handling_'.$zz_conf['error_handling']] = true;
 	}
-	$data['error_mail_level'] = is_array($zz_conf['error_mail_level']) ? implode(', ', $zz_conf['error_mail_level']) : $zz_conf['error_mail_level'];
+	$data['error_mail_level'] = is_array($zz_conf['error_mail_level'])
+		? implode(', ', $zz_conf['error_mail_level']) : $zz_conf['error_mail_level'];
 	
 	if ($zz_conf['log_errors']) {
 		$data['logfiles'] = [];
+
+		// PHP logfile?
 		if ($php_log = ini_get('error_log')) {
 			$php_log = realpath($php_log);
 			$data['logfiles'][$php_log]['path'] = $php_log;
 			$data['logfiles'][$php_log]['title'][] = 'PHP';
 		}
+		// zzform, zzwrap logfiles?
 		$levels = ['error', 'warning', 'notice'];
 		foreach ($levels as $level) {
 			if ($zz_conf['error_log'][$level]) {
 				$logfile = realpath($zz_conf['error_log'][$level]);
 				if (!$logfile) continue;
 				$data['logfiles'][$logfile]['path'] = $logfile;
+				$data['logfiles'][$logfile]['log'] = basename($logfile);
 				$data['logfiles'][$logfile]['title'][] = ucfirst($level);
 			}
 		}
 		$data['logfiles'] = array_values($data['logfiles']);
 		foreach ($data['logfiles'] as $index => $logfile) {
 			$data['logfiles'][$index]['title'] = implode(', ', $logfile['title']);
+			if (str_starts_with($logfile['path'], $zz_setting['cms_dir']))
+				$data['logfiles'][$index]['inside_log_dir'] = true;
 		}
 	}
 
