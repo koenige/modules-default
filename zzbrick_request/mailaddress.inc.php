ed6d17747491dd65647e8ddb91bd4dce5f6b38cb
Gustaf Mossakowski <gustaf@koenige.org>
2022-01-20 16:24:50 +0000

    mailaddress: add script to output e-mail address, using own_e_mail and own_name if no name or e-mail address are given
    git-svn-id: file:///Repos/modules/default/trunk@484 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_request/mailaddress.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * default module
+ * output mail address with name
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2016, 2021-2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * %%% request mailaddress %%%
+ * %%% request mailaddress "First Last" %%%
+ * %%% request mailaddress "First Last" test@example.org %%%
+ *
+ * @param array $params
+ * @return array
+ */
+function mod_default_mailaddress($params) {
+	global $zz_setting;
+	
+	$name = wrap_get_setting('own_name');
+	$mail = wrap_get_setting('own_e_mail');
+	
+	if (count($params) === 2) {
+		if (wrap_mail_valid($params[1])) {
+			$mail = $params[1];
+			$name = $params[0];
+		} elseif (wrap_mail_valid($params[0])) {
+			$mail = $params[0];
+			$name = $params[1];
+		} else {
+			$name = implode(' ', $params);
+		}
+	} elseif (count($params) === 1) {
+		if (wrap_mail_valid($params[0])) {
+			$mail = $params[0];
+		} else {
+			$name = $params[0];
+		}
+	} elseif (count($params)) {
+		return false;
+	}
+	if ($name) {
+		$page['text'] = wrap_mailto($name, $mail);
+	} else {
+		$page['text'] = markdown_inline(sprintf('<%s>', $mail));
+	}
+	return $page;
+}
