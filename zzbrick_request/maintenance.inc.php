46d750ccb0896b56206631b72c81d2d870902439
Gustaf Mossakowski <gustaf@koenige.org>
2022-03-09 16:35:53 +0000

    maintenance: code style; move check against key in query string to separate function for better readability
    git-svn-id: file:///Repos/modules/default/trunk@499 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_request/maintenance.inc.php
+++ zzbrick_request/maintenance.inc.php
@@ -77,9 +77,7 @@ function mod_default_maintenance($params) {
 		return zz_maintenance_ghostscript($page);
 	} elseif (isset($_GET['integrity'])) {
 		return zz_maintenance_integrity($page);
-	} elseif (isset($_GET['dbupdate']) OR isset($_GET['dbmodules'])) {
-		if (isset($_GET['dbupdate'])) $key = 'dbupdate';
-		elseif (isset($_GET['dbmodules'])) $key = 'dbmodules';
+	} elseif ($key = zz_maintenance_keycheck()) {
 		$newpage = brick_format('%%% make '.$key.' %%%');
 		$page['title'] .= ' '.$newpage['title'];
 		$page['text'] = $newpage['text'];
@@ -116,6 +114,19 @@ function mod_default_maintenance($params) {
 	$page['title'] .= ' '.wrap_text('Maintenance');
 	$page['breadcrumbs'][] = wrap_text('Maintenance');
 	return $page;
+}
+
+/**
+ * check script depending on GET variable
+ *
+ * @return string
+ */
+function zz_maintenance_keycheck() {
+	$keys = ['dbupdate', 'dbmodules'];
+	foreach ($keys as $key) {
+		if (isset($_GET[$key])) return $key;
+	}
+	return '';
 }
 
 /**
