1d24fd6399a6fe501a46b73921bb8033b3811479
Gustaf Mossakowski <gustaf@koenige.org>
2022-03-10 13:13:42 +0000

    translations check: add button to delete all orphan translations per translation field
    git-svn-id: file:///Repos/modules/default/trunk@504 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/translationscheck.inc.php
+++ zzbrick_make/translationscheck.inc.php
@@ -25,14 +25,27 @@ function mod_default_make_translationscheck($params) {
 	global $zz_setting;
 	global $zz_conf;
 	if (empty($zz_conf['translations_table'])) return false;
+	require_once $zz_conf['dir'].'/zzform.php';
+	
+	$to_delete = false;
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		foreach (array_keys($_POST) as $key) {
+			if (!str_starts_with($key, 'field_id_')) continue;
+			$to_delete = substr($key, 9);
+		}
+	}
 	
 	$sql = 'SELECT translationfield_id, db_name, table_name, field_name, field_type
 		FROM %s
+		%s
 		ORDER BY translationfield_id';
-	$sql = sprintf($sql, $zz_conf['translations_table']);
+	$sql = sprintf($sql
+		, $zz_conf['translations_table']
+		, $to_delete ? sprintf('WHERE translationfield_id = %d', $to_delete) : ''
+	);
 	$fields = wrap_db_fetch($sql, ['db_name', 'translationfield_id']);
 
-	$sql_t = 'SELECT COUNT(*)
+	$sql_t = 'SELECT %s
 		FROM _translations_%s translations
 		LEFT JOIN %s.%s data_table
 			ON translations.field_id = data_table.%s
@@ -51,6 +64,7 @@ function mod_default_make_translationscheck($params) {
 		$primary_keys = wrap_db_fetch($sql, '_dummy_', 'key/value');
 		foreach ($fields as $field) {
 			$sql = sprintf($sql_t
+				, $to_delete ? 'translation_id' : 'COUNT(*)'
 				, $field['field_type']
 				, $field['db_name']
 				, $field['table_name']
@@ -58,16 +72,29 @@ function mod_default_make_translationscheck($params) {
 				, $field['translationfield_id']
 				, $primary_keys[$field['table_name']]
 			);
-			$data[$database]['database'] = $database;
-			$data[$database]['tables'][$field['translationfield_id']] = $field;
-			$data[$database]['tables'][$field['translationfield_id']]['records'] = wrap_db_fetch($sql, '', 'single value');
-			$data[$database]['tables'][$field['translationfield_id']]['path'] = wrap_path('default_tables', 'translations-'.$field['field_type']);
+			if ($to_delete) {
+				$deletable_ids = wrap_db_fetch($sql, '_dummy_', 'single value');
+				foreach ($deletable_ids as $id) {
+					$values = [];
+					$values['action'] = 'delete';
+					$values['POST']['translation_id'] = $id;
+					$ops = zzform_multi('translations-'.$field['field_type'], $values);
+				}
+				return wrap_redirect_change(sprintf('?deleted=%d', count($deletable_ids)));
+			} else {
+				$data[$database]['database'] = $database;
+				$data[$database]['tables'][$field['translationfield_id']] = $field;
+				$data[$database]['tables'][$field['translationfield_id']]['records'] = wrap_db_fetch($sql, '', 'single value');
+				$data[$database]['tables'][$field['translationfield_id']]['path'] = wrap_path('default_tables', 'translations-'.$field['field_type']);
+			}
 		}
 	}
 	$data = array_values($data);
+	if (!empty($_GET['deleted'])) $data['deleted'] = $_GET['deleted'];
 	mysqli_select_db($zz_conf['db_connection'], $current_db);
 
 	$page['text'] = wrap_template('translationscheck', $data);
+	$page['query_strings'][] = 'deleted';
 	$page['title'] = wrap_text('Check Translations');
 	$page['breadcrumbs'][] = wrap_text('Check Translations');
 	return $page;
