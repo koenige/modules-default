249d5830ca01dfc9e52c7a92a8014a9e99bae5ff
Gustaf Mossakowski <gustaf@koenige.org>
2022-08-01 15:08:18 +0000

    search: add search through webpages; ignore pages that are authenticated or have search=0; show opengraph.png if no image for webpage is given
    git-svn-id: file:///Repos/modules/default/trunk@549 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ default/search.inc.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * default module
+ * search functions
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+function mf_default_search($q) {
+	global $zz_setting;
+	$where_sql = '(title LIKE "%%%s%%"
+		OR content LIKE "%%%s%%"
+		OR description LIKE "%%%s%%")';
+	$where = [];
+	foreach ($q as $string) {
+		$where[] = sprintf($where_sql, $string, $string, $string);
+	}
+	
+	$t_where = mod_default_search_translations($q, [
+		'webpages' => [
+			'id_field_name' => 'page_id',
+			'fields' => ['title', 'content', 'description']
+		]
+	]);
+
+	$sql = 'SELECT page_id
+			, CONCAT(identifier, IF(STRCMP(ending, "none"), ending, "")) AS identifier
+			, title, description
+		FROM webpages
+		WHERE (%s
+		%s)
+		AND live = "yes"
+		AND identifier NOT LIKE "%%*"
+		AND (ISNULL(parameters) OR parameters NOT LIKE "%%&search=0%%")
+		ORDER BY identifier';
+	$sql = sprintf($sql
+		, implode(' AND ', $where)
+		, $t_where ? ' OR '.$t_where : ''
+	);
+	$data['default'][0]['webpages'] = wrap_db_fetch($sql, 'page_id');
+	foreach ($data['default'][0]['webpages'] as $page_id => $page) {
+		foreach ($zz_setting['auth_urls'] as $url) {
+			if (!str_starts_with($page['identifier'], $url)) continue;
+			unset($data['default'][0]['webpages'][$page_id]);
+		}
+	}
+	$data['default'][0]['webpages'] = wrap_translate($data['default'][0]['webpages'], 'webpages');
+	$data['default'][0]['webpages'] = mf_default_webpages_media($data['default'][0]['webpages']);
+	return $data;
+}
+
+function mf_default_webpages_media($data) {
+	global $zz_setting;
+	static $opengraph_img;
+	if (!$data) return [];
+	$media = wrap_get_media(array_keys($data), 'webpages', 'page');
+	foreach ($media as $id => $files) {
+		$data[$id] += $files;
+	}
+	foreach ($data as $id => $line) {
+		if (!empty($line['images'])) continue;
+		if (empty($zz_setting['active_theme'])) continue;
+		if (empty($opengraph_img)) {
+			$filename = sprintf('%s/%s/opengraph.png', $zz_setting['themes_dir'], $zz_setting['active_theme']);
+			if (!file_exists($filename)) break;
+			$opengraph_img = [
+				'filename' => 'opengraph',
+				'thumb_extension' => 'png',
+				'no_image_size' => 1,
+				'no_'
+			];
+		}
+		$data[$id]['images'][] = $opengraph_img;
+		
+	}
+	return $data;
+}
