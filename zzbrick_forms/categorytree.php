5a9ab2328b9d6d0c226335ca091aed3b0f5c34e0
Gustaf Mossakowski <gustaf@koenige.org>
2022-09-30 17:56:08 +0000

    categorytree: add new form to show only one branch of the catgory tree; allow settings title, set_redirect_old, set_redirect_new, set_redirect_path
    git-svn-id: file:///Repos/modules/default/trunk@575 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick_forms/categorytree.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * default module
+ * Database form for a tree of categories
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+if (!$category_id = wrap_category_id($brick['vars'][0]))
+	wrap_quit(404);
+
+// @todo add access restrictions
+
+$zz = zzform_include_table('categories');
+if (!empty($brick['local_settings']['title']))
+	$zz['title'] = $brick['local_settings']['title'];
+
+$zz['fields'][4]['show_hierarchy_subtree'] = $category_id;
+$zz['fields'][4]['show_hierarchy_use_top_value_instead_NULL'] = true;
+
+$zz['list']['hierarchy']['id'] = $category_id;
+$zz['list']['hierarchy']['hide_top_value'] = true;
+
+unset($zz['filter']);
+
+// @todo details
+
+if (!empty($brick['local_settings']['set_redirect_old']))
+	$zz['set_redirect'][] = [
+		'old' => $brick['local_settings']['set_redirect_old'],
+		'new' => $brick['local_settings']['set_redirect_new'] ?? $brick['local_settings']['set_redirect_old'],
+		'field_name' => $brick['local_settings']['set_redirect_field'] ?? 'path'
+	];
