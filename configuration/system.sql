4481343796d8bfe7daf439b8bd9f6ee3257c3931
Gustaf Mossakowski <gustaf@koenige.org>
2022-10-21 10:41:34 +0000

    login-request/addlogin: move template and translations to contacts module; move some auth-queries dependent on contacts module, too
    git-svn-id: file:///Repos/modules/default/trunk@591 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- configuration/system.sql
+++ configuration/system.sql
@@ -70,34 +70,6 @@ SELECT password, username, logins.login_id AS user_id, logins.login_id
 FROM /*_PREFIX_*/logins logins
 WHERE active = 'yes' AND username = _latin1'%s';
 
--- auth_login_contact --
-SELECT password, identifier AS username, contacts.contact_id AS user_id, logins.login_id
-FROM /*_PREFIX_*/logins logins
-LEFT JOIN /*_PREFIX_*/contacts contacts USING (contact_id)
-WHERE active = 'yes' AND identifier = _latin1'%s';
-
--- auth_login_email --
-SELECT password
-, (SELECT identification FROM /*_PREFIX_*/contactdetails cd
-WHERE cd.contact_id = contacts.contact_id
-AND provider_category_id = /*_ID CATEGORIES provider/e-mail _*/ ORDER BY identification LIMIT 1) AS username
-, contacts.contact_id AS user_id, logins.login_id
-FROM /*_PREFIX_*/logins logins
-LEFT JOIN /*_PREFIX_*/contacts contacts USING (contact_id)
-WHERE active = 'yes'
-HAVING username = _latin1'%s';
-
--- auth_login_exists --
-SELECT login_id
-FROM /*_PREFIX_*/logins
-LEFT JOIN /*_PREFIX_*/contacts USING (contact_id)
-WHERE /*_PREFIX_*/contacts.identifier = '%s';
-
--- auth_username_exists --
-SELECT contact_id AS user_id, identifier AS username
-FROM /*_PREFIX_*/contacts
-WHERE identifier = '%s';
-
 -- auth_last_masquerade --
 
 -- auth_login_masquerade --
