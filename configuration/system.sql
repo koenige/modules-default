45c80ea7c1f9c1e32a0f27099da8d5538b826040
Gustaf Mossakowski <gustaf@koenige.org>
2022-11-17 15:38:21 +0000

    system.sql: remove zzform_filetypelist, now in zzform module
    git-svn-id: file:///Repos/modules/default/trunk@605 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- configuration/system.sql
+++ configuration/system.sql
@@ -97,7 +97,3 @@ SELECT country_code, country_id FROM /*_PREFIX_*/countries;
 
 -- ids_folders --
 SELECT filename, medium_id FROM /*_PREFIX_*/media WHERE filetype_id = /*_ID FILETYPES folder */;
-
-
--- zzform_filetypelist --
-SELECT filetype_id, UPPER(filetype) AS filetype, filetype_description FROM /*_PREFIX_*/filetypes WHERE filetype IN ('%s');
