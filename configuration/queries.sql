44de67f1403ffe7dd6609f28a0d2b09673655b8b
Gustaf Mossakowski <gustaf@koenige.org>
2022-11-17 16:15:10 +0000

    text: use query default_text__table instead of $zz_conf['text_table']
    git-svn-id: file:///Repos/modules/default/trunk@608 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ configuration/queries.sql
@@ -0,0 +1,15 @@
+/**
+ * default module
+ * SQL queries
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/default
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+-- default_text__table --
+/* text */
