2ca14976802372204e928e5b2fbb67d05fb0091f
Gustaf Mossakowski <gustaf@koenige.org>
2023-02-08 19:07:53 +0000

    websites: add link below domain with `default_website`, change order of fields
    git-svn-id: file:///Repos/modules/default/trunk@664 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/websites.php
+++ zzbrick_tables/websites.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2016-2017, 2019-2021 Gustaf Mossakowski
+ * @copyright Copyright © 2016-2017, 2019-2021, 2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -20,11 +20,15 @@ $zz['fields'][1]['title'] = 'ID';
 $zz['fields'][1]['field_name'] = 'website_id';
 $zz['fields'][1]['type'] = 'id';
 
-$zz['fields'][2]['field_name'] = 'website';
-$zz['fields'][2]['typo_remove_double_spaces'] = true;
-
 $zz['fields'][3]['field_name'] = 'domain';
 $zz['fields'][3]['explanation'] = 'Domain name without subdomain www';
+$zz['fields'][3]['link'] = [
+	'area' => 'default_website',
+	'fields' => ['domain']
+];
+
+$zz['fields'][2]['field_name'] = 'website';
+$zz['fields'][2]['typo_remove_double_spaces'] = true;
 
 $zz['sql'] = 'SELECT websites.*
 	FROM websites
