c7577bef0c95e20715f975e9703f5645b31f401c
Gustaf Mossakowski <gustaf@koenige.org>
2023-06-13 04:18:24 +0000

    subtable categories: set suffix of subselect; allow to set `hide_in_list`
    git-svn-id: file:///Repos/modules/default/trunk@776 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/_subtable_categories.inc.php
+++ zzbrick_tables/_subtable_categories.inc.php
@@ -69,7 +69,9 @@ function mf_default_categories_subtable(&$zz, $table, $path, $start_no) {
 		$zz['fields'][$no]['title'] = $category['category'];
 		$zz['fields'][$no]['table_name'] = $table.'_categories_'.$category['category_id'];
 		$zz['fields'][$no]['unless']['export_mode']['subselect']['prefix'] = '<br><em>'.wrap_text($category['category']).': ';
+		$zz['fields'][$no]['unless']['export_mode']['subselect']['suffix'] = '';
 		$zz['fields'][$no]['form_display'] = $category['form_display'] ?? 'lines';
+		$zz['fields'][$no]['hide_in_list'] = $category['hide_in_list'] ?? false;
 		if (isset($category['min_records']))
 			$zz['fields'][$no]['min_records'] = $category['min_records'];
 		elseif ($zz['fields'][$no]['form_display'] === 'set')
