2e4da7d0792ab8976f19e25a495196d49add567d
Gustaf Mossakowski <gustaf@koenige.org>
2023-07-25 11:45:52 +0000

    log: copyright date
    git-svn-id: file:///Repos/modules/default/trunk@813 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/log.inc.php
+++ zzbrick_make/log.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2022-2023 Gustaf Mossakowski
+ * @copyright Copyright © 2010, 2013-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
