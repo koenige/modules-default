512cdc880d04c2ed32bb53153c63cbd5b9bd4885
Gustaf Mossakowski <gustaf@koenige.org>
2023-09-09 08:58:33 +0000

    add function mf_default_category_hierarchy(), returns a category tree hierarchically sorted, with all category names of levels above
    git-svn-id: file:///Repos/modules/default/trunk@834 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- default/_functions.inc.php
+++ default/_functions.inc.php
@@ -59,3 +59,40 @@ function mf_default_logfiles() {
 	
 	return $logfiles;
 }
+
+/**
+ * get category tree hierarchy
+ *   array hierarchy, id => level
+ *   array categories, id => category_id, main_category_id, category, category_level_0 to n
+ *
+ * @param string $path
+ * @return array
+ */
+function mf_default_category_hierarchy($path) {	
+	$ids = wrap_category_id($path, 'list');
+	$sql = 'SELECT category_id, main_category_id, category
+		FROM categories
+		WHERE category_id IN (%s)
+		ORDER BY main_category_id, sequence';
+	$sql = sprintf($sql, implode(',', $ids));
+	$categories = wrap_db_fetch($sql, 'category_id');
+	$categories = wrap_translate($categories, 'categories');
+
+	// get IDs sorted by hierarchy
+	$hierarchy = wrap_hierarchy($categories, 'main_category_id', wrap_category_id($path));
+
+	// get categories sorted by hierarchy
+	$data = [];
+	foreach ($hierarchy as $category_id => $level) {
+		$category = $categories[$category_id];
+		$category['level'] = $level;
+		$category['category_level_'.$level] = $category['category'];
+		$main_category = $categories[$category['main_category_id']] ?? [];
+		for ($i = $level - 1; $i >= 0; $i--) {
+			$category['category_level_'.$i] = $main_category['category'];
+			$main_category = $categories[$main_category['main_category_id']] ?? [];
+		}
+		$data[$category_id] = $category;
+	}
+	return $data;
+}
