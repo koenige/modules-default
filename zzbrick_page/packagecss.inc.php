3cc1482616eeba124e6c7e82e33794487756e978
Gustaf Mossakowski <gustaf@koenige.org>
2024-08-01 12:53:16 +0000

    packagecss: allow to use further CSS files via $page['extra']['css'] = 'package/file.css'
    git-svn-id: file:///Repos/modules/default/trunk@944 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_page/packagecss.inc.php
+++ zzbrick_page/packagecss.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2022-2023 Gustaf Mossakowski
+ * @copyright Copyright © 2022-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -18,8 +18,20 @@
  *
  * @return string
  */
-function page_packagecss() {
-	if (!wrap_setting('activated')) return '';
+function page_packagecss($params, $page) {
+	$css = [];
+	$extra_css = $page['extra']['css'] ?? [];
+	if (!is_array($extra_css)) $extra_css = [$extra_css];
+	foreach ($extra_css as $line) {
+		list($package, $filename) = explode('/', $line);
+		if (str_ends_with($filename, '.css'))
+			$filename = substr($filename, 0, -4);
+		$css[] = [
+			'package' => $package,
+			'filename' => $filename
+		];
+	}
+
 	$activated = wrap_setting('activated');
 	ksort($activated); // first modules, then themes
 
@@ -27,7 +39,6 @@ function page_packagecss() {
 	if (!empty($activated['themes']) AND count($activated['themes']) > 1)
 		$activated['themes'] = [$activated['themes'][0]];
 
-	$css = [];
 	foreach ($activated as $type => $packages) {
 		if (!$packages) continue; // 400 bad request etc.
 		foreach ($packages as $package) {
@@ -35,7 +46,10 @@ function page_packagecss() {
 			if ($package === 'default') continue;
 			$file = wrap_collect_files('layout/'.$package.'.css', $package);
 			if (!$file) continue;
-			$css[]['filename'] = $package;
+			$css[] = [
+				'package' => $package,
+				'filename' => $package
+			];
 		}
 	}
 	return wrap_template('packagecss', $css);
