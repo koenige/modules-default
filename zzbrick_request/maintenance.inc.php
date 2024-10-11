81f42a2f66d3d8ceb63d021693f87e6fb663bcb4
Gustaf Mossakowski <gustaf@koenige.org>
2024-10-11 10:29:37 +0000

    maintenance: move functions zz_maintenance_searched(), zz_maintenance_deleteall_form(), zz_maintenance_list_init() to module functions
    git-svn-id: file:///Repos/modules/default/trunk@973 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_request/maintenance.inc.php
+++ zzbrick_request/maintenance.inc.php
@@ -435,53 +435,6 @@ function zz_maintenance_folders($page = []) {
 	return $page;
 }
 
-/**
- * checks if 'search' is active, then tests string against search string
- *
- * @param string $string
- * @return bool false: no search active or string not found; true: search is
- *		active and string was found
- */
-function zz_maintenance_searched($string) {
-	if (empty($_GET['q'])) return false;
-	if (stristr($string, $_GET['q'])) return true;
-	if (stristr(urldecode($string), $_GET['q'])) return true;
-
-	// allow for searching ignoring replaced zero width space
-	// PHPs char does not support unicode
-	$q = urlencode($_GET['q']);
-	if (wrap_setting('character_set') === 'utf-8') {
-		$char8203 = '%E2%80%8B';
-	} else {
-		// this does not work for all other character sets
-		// but it works for iso-8859-1 and -2
-		$char8203 = '%26%238203%3B';
-	}
-	$q = str_replace($char8203, '', $q);
-	$q = urldecode($q);
-	$_GET['q'] = $q;
-	if (stristr($string, $q)) return true;
-	return false;
-}
-
-/**
- * HTML output of form with button to delete all lines, files etc. in list
- * 'q'-search filter will be regarded
- *
- * @global array $zz_conf
- * @return array
- */
-function zz_maintenance_deleteall_form() {
-	global $zz_conf;
-	if (!empty($_POST['deleteall'])) return ['', ''];
-	if (!isset($_GET['deleteall'])) return ['', ''];
-
-	$unwanted_keys = ['deleteall'];
-	$qs = zz_edit_query_string($zz_conf['int']['url']['qs_zzform'], $unwanted_keys);
-	$url = $zz_conf['int']['url']['full'].$qs;
-	return [$url, !empty($_GET['q']) ? wrap_html_escape($_GET['q']) : ''];
-}
-
 function zz_maintenance_folders_deleteall($my_folder, $file) {
 	if (!empty($_GET['q'])) {
 		if (zz_maintenance_searched($file)) {
@@ -493,32 +446,6 @@ function zz_maintenance_folders_deleteall($my_folder, $file) {
 		return $success;
 	}
 }
-
-/**
- * initialize variables and include files to use zz_list() for maintenance
- *
- */
-function zz_maintenance_list_init() {
-	global $zz_conf;
-	static $init = false;
-	if ($init) return; // just once
-
-	// zz_edit_query_string(), zz_get_url_self()
-	wrap_include('functions', 'zzform');
-	// zz_init_limit()
-	wrap_include('output', 'zzform');
-	// zz_mark_search_string(), zz_list_total_records(), zz_list_pages()
-	wrap_include('list', 'zzform');
-	// zz_search_form()
-	wrap_include('searchform', 'zzform');
-
-	wrap_setting('zzform_search', 'bottom');
-
-	$zz_conf['int']['url'] = zz_get_url_self();
-	zz_init_limit();
-	$init = true;
-}
-
 
 /**
  * format a single line from log
