f41d2d955ef2fc6a7317193561650cffac403d69
Gustaf Mossakowski <gustaf@koenige.org>
2025-01-28 13:44:37 +0000

    dbimport: show different records, one by one and let user decide whether it is identical or different, save result in logfile
    git-svn-id: file:///Repos/modules/default/trunk@1068 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_make/dbimport.inc.php
+++ zzbrick_make/dbimport.inc.php
@@ -46,6 +46,10 @@ function mod_default_make_dbimport() {
 }
 
 function mod_default_dbimport_table($data, $log) {
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		mod_default_dbimport_table_save();
+	}
+
 	$tabledata = [];
 	$conditions = [];
 	$data['table'] = $_GET['table'];
@@ -83,7 +87,7 @@ function mod_default_dbimport_table($data, $log) {
 	$data['records_different'] = [];
 	foreach ($tabledata as $record_id => $line) {
 		if (!array_key_exists($record_id, $existing)) {
-			mod_default_dbimport_log($data['table'], $record_id);
+			mod_default_dbimport_log($data['table'], 'write', $record_id);
 			$data['new']++;
 			$data['records_new'][$record_id] = $line;
 			continue;
@@ -91,11 +95,23 @@ function mod_default_dbimport_table($data, $log) {
 		unset($line['last_update']);
 		unset($existing[$record_id]['last_update']);
 		if ($line === $existing[$record_id]) {
-			mod_default_dbimport_log($data['table'], $record_id, $record_id);
+			mod_default_dbimport_log($data['table'], 'write', $record_id, $record_id);
 			$data['identical']++;
 		} else {
 			$data['different']++;
-			// @todo show what is different, old vs. new record
+			// show what is different, old vs. new record, just one per time
+			if (empty($data['diff']) AND mod_default_dbimport_log($data['table'], 'check', $record_id)) {
+				$data['diff'] = [];
+				$data['diff_record_id'] = $record_id;
+				foreach ($line as $field_name => $value) {
+					$data['diff'][] = [
+						'field' => $field_name,
+						'new_value' => $value,
+						'old_value' => $existing[$record_id][$field_name] ?? NULL,
+						'identical' => ($value.'' === $existing[$record_id][$field_name].'' ?? NULL) ? true : false
+					];
+				}
+			}
 			$data['records_different'][$record_id] = $line;
 		}
 	}
@@ -114,10 +130,12 @@ function mod_default_dbimport_table($data, $log) {
  * save ID matching in logfile
  *
  * @param string $table
+ * @param string $action
  * @param int $old_record_id
- * @param int $new_record_id
+ * @param int $new_record_id (optional)
+ * @return bool false: record ID exists, true: record ID does not exist
  */
-function mod_default_dbimport_log($table, $old_record_id, $new_record_id = 0) {
+function mod_default_dbimport_log($table, $action, $old_record_id, $new_record_id = 0) {
 	static $increment = 0;
 	if (!$increment) $increment = wrap_db_increment($table);
 
@@ -126,12 +144,25 @@ function mod_default_dbimport_log($table, $old_record_id, $new_record_id = 0) {
 	if (!$new_record_id) {
 		// already in log?
 		foreach ($log as $line) {
-			if ($line['old_record_id'].'' === $old_record_id.'') return;
+			if ($line['old_record_id'].'' === $old_record_id.'') return false;
 			if ($line['new_record_id'] >= $increment)
 				$increment = ++$line['new_record_id'];
 		}
 		$new_record_id = $increment++;
 	}
-	
-	wrap_file_log($logfile, 'write', [time(), $old_record_id, $new_record_id]);
+
+	if ($action === 'write')
+		wrap_file_log($logfile, 'write', [time(), $old_record_id, $new_record_id]);
+	return true;
+}
+
+/**
+ * manually save ID matching in logfile
+ *
+ */
+function mod_default_dbimport_table_save() {
+	if (array_key_exists('identical', $_POST))
+		mod_default_dbimport_log($_GET['table'], 'write', $_POST['record_id'], $_POST['record_id']);
+	else
+		mod_default_dbimport_log($_GET['table'], 'write', $_POST['record_id']);
 }
