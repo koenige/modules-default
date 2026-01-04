ec417e9fcfe54d769267b76a99a1277f761e570a
Gustaf Mossakowski <gustaf@koenige.org>
2026-01-04 14:27:32 +0000

    translations: use $zz notation, not $zz_sub
    git-svn-id: file:///Repos/modules/default/trunk@1262 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick_tables/translations-varchar.php
+++ zzbrick_tables/translations-varchar.php
@@ -9,83 +9,83 @@
  * https://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2010-2011, 2013, 2018-2020, 2022-2025 Gustaf Mossakowski
+ * @copyright Copyright © 2010-2011, 2013, 2018-2020, 2022-2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
-$zz_sub['title'] = 'Translation';
-$zz_sub['show_title'] = false;
-$zz_sub['type'] = 'subtable';
-$zz_sub['hide_in_list'] = true;
-$zz_sub['min_records'] = 1;
-$zz_sub['max_records_sql'] = 'SELECT COUNT(*) FROM /*_PREFIX_*/languages 
+$zz['title'] = 'Translation';
+$zz['show_title'] = false;
+$zz['type'] = 'subtable';
+$zz['hide_in_list'] = true;
+$zz['min_records'] = 1;
+$zz['max_records_sql'] = 'SELECT COUNT(*) FROM /*_PREFIX_*/languages 
 	WHERE (iso_639_1 <> "/*_SETTING default_source_language _*/" OR NOT ISNULL(variation))
 	AND website = "yes"';
-$zz_sub['form_display'] = 'lines';
+$zz['form_display'] = 'lines';
 
-$zz_sub['table'] = '/*_PREFIX_*/_translations_varchar';
-$zz_sub['table_name'] = 'translations_text';
+$zz['table'] = '/*_PREFIX_*/_translations_varchar';
+$zz['table_name'] = 'translations_text';
 // just show record if condition 99 is true (or undefined)
 // nur Anzeigen des Datensatzes bei Bedingung 99
-$zz_sub['unless'][99]['access'] = 'show';
+$zz['unless'][99]['access'] = 'show';
 
-$zz_sub['subselect']['sql'] = 'SELECT field_id, CONCAT(iso_639_1, IFNULL(CONCAT("-", variation), "")) AS lang,
+$zz['subselect']['sql'] = 'SELECT field_id, CONCAT(iso_639_1, IFNULL(CONCAT("-", variation), "")) AS lang,
 		translation
 	FROM /*_PREFIX_*/_translations_varchar
 	LEFT JOIN /*_PREFIX_*/languages USING (language_id)';
-$zz_sub['subselect']['concat_fields'] = ': ';
+$zz['subselect']['concat_fields'] = ': ';
 
-$zz_sub['fields'][1]['title'] = 'ID';
-$zz_sub['fields'][1]['field_name'] = 'translation_id';
-$zz_sub['fields'][1]['type'] = 'id';
+$zz['fields'][1]['title'] = 'ID';
+$zz['fields'][1]['field_name'] = 'translation_id';
+$zz['fields'][1]['type'] = 'id';
 
-$zz_sub['fields'][2]['title'] = 'Translation field';
-$zz_sub['fields'][2]['field_name'] = 'translationfield_id';
-$zz_sub['fields'][2]['type'] = 'translation_key';
-$zz_sub['fields'][2]['type_detail'] = 'select';
-$zz_sub['fields'][2]['sql'] = 'SELECT translationfield_id
+$zz['fields'][2]['title'] = 'Translation field';
+$zz['fields'][2]['field_name'] = 'translationfield_id';
+$zz['fields'][2]['type'] = 'translation_key';
+$zz['fields'][2]['type_detail'] = 'select';
+$zz['fields'][2]['sql'] = 'SELECT translationfield_id
 		, CONCAT(db_name, " | ", table_name, " | ", field_name) AS translationfield
 	FROM /*_TABLE default_translationfields _*/
 	WHERE field_type = "varchar"';
-$zz_sub['fields'][2]['display_field'] = 'translationfield';
-$zz_sub['fields'][2]['exclude_from_search'] = true;
-$zz_sub['fields'][2]['if']['where']['hide_in_list'] = true;
+$zz['fields'][2]['display_field'] = 'translationfield';
+$zz['fields'][2]['exclude_from_search'] = true;
+$zz['fields'][2]['if']['where']['hide_in_list'] = true;
 
-$zz_sub['fields'][3]['title'] = 'ID';
-$zz_sub['fields'][3]['field_name'] = 'field_id';
-$zz_sub['fields'][3]['type'] = 'foreign_key';
+$zz['fields'][3]['title'] = 'ID';
+$zz['fields'][3]['field_name'] = 'field_id';
+$zz['fields'][3]['type'] = 'foreign_key';
 
-$zz_sub['fields'][5]['field_name'] = 'language_id';
-$zz_sub['fields'][5]['type'] = 'select';
-$zz_sub['fields'][5]['sql'] = 'SELECT language_id, language, variation
+$zz['fields'][5]['field_name'] = 'language_id';
+$zz['fields'][5]['type'] = 'select';
+$zz['fields'][5]['sql'] = 'SELECT language_id, language, variation
 	FROM /*_PREFIX_*/languages 
 	WHERE iso_639_1 <> "/*_SETTING default_source_language _*/"
 	AND website = "yes"
 	ORDER BY language';
-$zz_sub['fields'][5]['sql_translate'] = ['language_id' => 'languages'];
-$zz_sub['fields'][5]['prefix'] = wrap_text('Translation to').' ';
-$zz_sub['fields'][5]['suffix'] = ': ';
-$zz_sub['fields'][5]['default'] = wrap_language_id(wrap_setting('default_translation_language'));
-$zz_sub['fields'][5]['def_val_ignore'] = true;
-$zz_sub['fields'][5]['show_title'] = false;
-$zz_sub['fields'][5]['append_next'] = true;
-$zz_sub['fields'][5]['display_field'] = 'lang';
-$zz_sub['fields'][5]['search'] = 'CONCAT(iso_639_1, IFNULL(CONCAT("-", variation), ""))';
-$zz_sub['fields'][5]['character_set'] = 'utf8';
-$zz_sub['fields'][5]['exclude_from_search'] = true;
+$zz['fields'][5]['sql_translate'] = ['language_id' => 'languages'];
+$zz['fields'][5]['prefix'] = wrap_text('Translation to').' ';
+$zz['fields'][5]['suffix'] = ': ';
+$zz['fields'][5]['default'] = wrap_language_id(wrap_setting('default_translation_language'));
+$zz['fields'][5]['def_val_ignore'] = true;
+$zz['fields'][5]['show_title'] = false;
+$zz['fields'][5]['append_next'] = true;
+$zz['fields'][5]['display_field'] = 'lang';
+$zz['fields'][5]['search'] = 'CONCAT(iso_639_1, IFNULL(CONCAT("-", variation), ""))';
+$zz['fields'][5]['character_set'] = 'utf8';
+$zz['fields'][5]['exclude_from_search'] = true;
 
-$zz_sub['fields'][4]['title'] = 'Translation';
-$zz_sub['fields'][4]['show_title'] = false;
-$zz_sub['fields'][4]['field_name'] = 'translation';
-$zz_sub['fields'][4]['inherit_format'] = true;
-$zz_sub['fields'][4]['unless'][1]['list_prefix'] = '<del>';
-$zz_sub['fields'][4]['unless'][1]['list_suffix'] = '</del>';
+$zz['fields'][4]['title'] = 'Translation';
+$zz['fields'][4]['show_title'] = false;
+$zz['fields'][4]['field_name'] = 'translation';
+$zz['fields'][4]['inherit_format'] = true;
+$zz['fields'][4]['unless'][1]['list_prefix'] = '<del>';
+$zz['fields'][4]['unless'][1]['list_suffix'] = '</del>';
 
-$zz_sub['subtitle']['translationfield_id']['sql'] = $zz_sub['fields'][2]['sql'];
-$zz_sub['subtitle']['translationfield_id']['var'] = ['translationfield'];
+$zz['subtitle']['translationfield_id']['sql'] = $zz['fields'][2]['sql'];
+$zz['subtitle']['translationfield_id']['var'] = ['translationfield'];
 
-$zz_sub['sql'] = 'SELECT /*_PREFIX_*/_translations_varchar.*
+$zz['sql'] = 'SELECT /*_PREFIX_*/_translations_varchar.*
 		, CONCAT(iso_639_1, IFNULL(CONCAT("-", variation), "")) AS lang
 		, CONCAT(db_name, " | ", table_name, " | ", field_name) AS translationfield
 	FROM /*_PREFIX_*/_translations_varchar
@@ -96,7 +96,7 @@ $zz_sub['sql'] = 'SELECT /*_PREFIX_*/_translations_varchar.*
 ';
 
 if (empty($_GET['order']) OR $_GET['order'] === 'translationfield')
-	$zz_sub['list']['group'] = 'translationfield';
+	$zz['list']['group'] = 'translationfield';
 
 if (!empty($_GET['where']['translationfield_id'])) {
 	$sql = 'SELECT db_name, table_name, field_name, field_type
@@ -116,22 +116,22 @@ if (!empty($_GET['where']['translationfield_id'])) {
 		);
 		$key_field_name = wrap_db_fetch($sql, '', 'single value');
 	
-		$zz_sub['conditions'][1]['scope'] = 'query';
-		$zz_sub['conditions'][1]['sql'] = 'SELECT translation_id
+		$zz['conditions'][1]['scope'] = 'query';
+		$zz['conditions'][1]['sql'] = 'SELECT translation_id
 			FROM %s.%s source_table
 			LEFT JOIN /*_PREFIX_*/_translations_varchar
 				ON source_table.%s = /*_PREFIX_*/_translations_varchar.field_id';
-		$zz_sub['conditions'][1]['sql'] = sprintf($zz_sub['conditions'][1]['sql']
+		$zz['conditions'][1]['sql'] = sprintf($zz['conditions'][1]['sql']
 			, $translation_field['db_name']
 			, $translation_field['table_name']
 			, $key_field_name
 		);
-		$zz_sub['conditions'][1]['key_field_name'] = 'translation_id';
+		$zz['conditions'][1]['key_field_name'] = 'translation_id';
 	}
 }
 
 // if not used as a subtable:
-$zz_sub['init_ignore_log'] = [
+$zz['init_ignore_log'] = [
 	'show_title', 'type', 'hide_in_list', 'min_records', 'max_records_sql', 'table_name',
 	'form_display'
 ];
