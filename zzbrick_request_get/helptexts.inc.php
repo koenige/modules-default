843205a217fe86004fab01e26cdf287712ad2b7f
Gustaf Mossakowski <gustaf@koenige.org>
2026-06-18 11:37:46 +0200

    helptexts: convert direct links to other Markdown files to identifier links without file ending, without language code

--- zzbrick_request_get/helptexts.inc.php
+++ zzbrick_request_get/helptexts.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/default
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2024-2025 Gustaf Mossakowski
+ * @copyright Copyright © 2024-2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -56,7 +56,7 @@ function mf_default_helptexts_files() {
 			$basename = implode('-', $basename);
 		}
 		$title = $basename;
-		$basename = wrap_filename(strtolower(wrap_normalize($basename)));
+		$basename = mf_default_helptexts_identifier($basename);
 		$data[$basename][] = [
 			'title' => $title,
 			'language' => $lang,
@@ -96,10 +96,78 @@ function mf_default_helptexts_content($file) {
 	$file['text'] = file_get_contents($file['filename']);
 	$file['text'] = preg_replace('/<!--[\s\S]*?-->/', '', $file['text']);
 	$file['text'] = preg_replace('/%%%(.*?)%%%/s', '%%% explain $1%%%', $file['text']);
+	$file['text'] = mf_default_helptexts_links($file['text']);
 
 	if ($file['type'] === 'md') {
 		preg_match('/# (.+)/', $file['text'], $matches);
 		if ($matches[1]) $file['title'] = $matches[1];
 	}
 	return $file;
+}
+
+/**
+ * helptext identifier from a file or link name (lowercase, no extension)
+ *
+ * @param string $name e.g. "Format.md", "Page Elements", "format"
+ * @return string e.g. "format", "page-elements"
+ */
+function mf_default_helptexts_identifier($name) {
+	$basename = basename($name);
+	$extension = wrap_file_extension($basename);
+	if ($extension)
+		$basename = substr($basename, 0, -strlen($extension) - 1);
+	if (strstr($basename, '-')) {
+		$parts = explode('-', $basename);
+		if (strlen(end($parts)) === 2) {
+			array_pop($parts);
+			$basename = implode('-', $parts);
+		}
+	}
+	return wrap_filename(strtolower(wrap_normalize($basename)));
+}
+
+/**
+ * convert markdown links to other help texts into site paths
+ *
+ * [Format.md](Format.md) and [Format](format) both link to the same page.
+ *
+ * @param string $text
+ * @return string
+ */
+function mf_default_helptexts_links($text) {
+	static $files = null;
+	if ($files === null)
+		$files = mf_default_helptexts_files();
+
+	return preg_replace_callback(
+		'/\[([^\]]+)\]\(([^)\s]+(?:\s+"[^"]*")?)\)/',
+		function ($match) use ($files) {
+			$link_text = $match[1];
+			$url = $match[2];
+			$title = '';
+			if (preg_match('/^([^\s]+)(?:\s+"([^"]*)")?$/', $url, $parts)) {
+				$url = $parts[1];
+				$title = $parts[2] ?? '';
+			}
+			if (preg_match('#^[a-z]+:#i', $url)) return $match[0];
+			if (str_starts_with($url, '/') || str_starts_with($url, '#')) return $match[0];
+
+			$anchor = '';
+			if (($pos = strpos($url, '#')) !== false) {
+				$anchor = substr($url, $pos);
+				$url = substr($url, 0, $pos);
+			}
+			$identifier = mf_default_helptexts_identifier($url);
+			if (!$identifier || !array_key_exists($identifier, $files)) return $match[0];
+
+			$path = wrap_path('default_helptext', $identifier);
+			if (!$path) return $match[0];
+
+			$output = sprintf('[%s](%s%s', $link_text, $path, $anchor);
+			if ($title) $output .= ' "'.$title.'"';
+			$output .= ')';
+			return $output;
+		},
+		$text
+	);
 }
