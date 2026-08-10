137e053e82f1ed996517ffd8968b0b58c773f5e5
Gustaf Mossakowski <gustaf@koenige.org>
2026-08-10 19:11:31 +0200

    logging: add field `db_table` to `_logging` table

--- configuration/install.sql
+++ configuration/install.sql
@@ -15,10 +15,12 @@
 CREATE TABLE `_logging` (
   `log_id` int unsigned NOT NULL AUTO_INCREMENT,
   `query` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `db_table` varchar(127) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `record_id` int unsigned DEFAULT NULL,
   `user` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`log_id`),
+  KEY `db_table` (`db_table`),
   KEY `record_id` (`record_id`),
   KEY `last_update` (`last_update`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
